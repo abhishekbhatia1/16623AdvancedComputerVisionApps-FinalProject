9620fbe94a4bfdcb6fd9fc2e0919d56a6620b1eb
Shiyu Dong <tjudsy@gmail.com>
2016-12-04 23:33:53 -0500

    add undistortion

--- pose_estimation/estimation.cpp
+++ pose_estimation/estimation.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 using namespace cv;
 
-#define MAX_FRAME 1000
+#define MAX_FRAME 15
 
 
 
@@ -25,6 +25,11 @@ int main(void) {
 
 	cv::Mat R_f, t_f;
 
+	Mat intrinsic = (Mat_<double>(3,3) << 593.09900, 0, 320.26862, 0, 590.09473, 237.86729, 0, 0, 1);
+	Mat distCoeffs = (Mat_<double>(1, 4) << 0.1571472287, -0.3774241507, -0.0006767344, 0.0022913516);
+
+	std::cout << "intrinsic = "<< std::endl << " "  << intrinsic << std::endl << std::endl;
+	std::cout << "distCoeffs = "<< std::endl << " "  << distCoeffs << std::endl << std::endl;
 
 	// char filename1[200];
 	//  	char filename2[200];
@@ -48,14 +53,37 @@ int main(void) {
 
 		std::cout << "numFrame: " << numFrame << std::endl;
 
-	  	sprintf(scenefile, "/home/shiyu/mono-vo/dataset/sequences/00/image_0/%06d.png", numFrame);
-	  	sprintf(objfile, "/home/shiyu/mono-vo/dataset/sequences/00/image_0/%06d.png", numFrame+1);
+	  	// sprintf(scenefile, "/home/shiyu/mono-vo/dataset/sequences/00/image_0/%06d.png", numFrame);
+	  	// sprintf(objfile, "/home/shiyu/mono-vo/dataset/sequences/00/image_0/%06d.png", numFrame+1);
+	  	// sprintf(scenefile, "/home/shiyu/16623AdvancedComputerVisionApps-FinalProject/data/IMG_%04d.JPG", numFrame+11);
+	  	// sprintf(objfile, "/home/shiyu/16623AdvancedComputerVisionApps-FinalProject/data/IMG_%04d.JPG", numFrame+12);
 
-	    cv::Mat imgScene = cv::imread(scenefile);
-	    cv::Mat imgObject = cv::imread(objfile);
+
+	  	sprintf(scenefile, "/home/shiyu/mono-vo/dataset/sequences/data/IMG_%04d.JPG", numFrame+26);
+	  	sprintf(objfile, "/home/shiyu/mono-vo/dataset/sequences/data/IMG_%04d.JPG", numFrame+27);
+
+	  	
+	    cv::Mat oriScene = cv::imread(scenefile);
+	    cv::Mat oriObject = cv::imread(objfile);
+
+	    cv::Mat imgScene;
+	    cv::Mat imgObject;
+
+
+	    resize(oriScene, oriScene, Size(640,480));
+	    resize(oriObject, oriObject, Size(640,480));
+
+
+	    undistort(oriScene, imgScene, intrinsic, distCoeffs);
+	    undistort(oriObject, imgObject, intrinsic, distCoeffs);
+
 	    
 
-	    
+
+	    if (imgScene.empty() || imgObject.empty()) {			    // if unable to open image
+	       std::cout << "error: image not read from file\n\n";		// show error message on command line
+	       return(0);												// and exit program
+		}
 
 	    cv::Ptr<cv::ORB> orb = cv::ORB::create();
 
@@ -95,19 +123,18 @@ int main(void) {
 		std::vector<float> err;
 		calcOpticalFlowPyrLK(imgScene,imgObject, scene, obj, status, err, Size(21,21), 3, TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01), 0, 0.001);
 
-  		// int indexCorrection = 0;
-		// for( int i=0; i<status.size(); i++)
-		//  {  Point2f pt = obj.at(i- indexCorrection);
-		//  	if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0))	{
-		//  		  if((pt.x<0)||(pt.y<0))	{
-		//  		  	status.at(i) = 0;
-		//  		  }
-		//  		  scene.erase (scene.begin() + (i - indexCorrection));
-		//  		  obj.erase (obj.begin() + (i - indexCorrection));
-		//  		  indexCorrection++;
-		//  	}
-
-		//  }
+  		int indexCorrection = 0;
+		for( int i=0; i<status.size(); i++)
+		 {  Point2f pt = obj.at(i- indexCorrection);
+		 	if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0))	{
+		 		  if((pt.x<0)||(pt.y<0))	{
+		 		  	status.at(i) = 0;
+		 		  }
+		 		  scene.erase (scene.begin() + (i - indexCorrection));
+		 		  obj.erase (obj.begin() + (i - indexCorrection));
+		 		  indexCorrection++;
+		 	}
+		 }
 
 		for(int j=0; j<obj.size(); j++){
                 if(status[j]){
@@ -117,38 +144,44 @@ int main(void) {
 
 
 	    //cv::Mat H = findHomography( obj, scene, CV_RANSAC);
-		double focal = 718.8560;
-	  	cv::Point2d pp(607.1928, 185.2157);
+		double focal = 578;
+	  	cv::Point2d pp(320, 240);
+	  	// double focal = 718.8560;
+	  	// cv::Point2d pp(607.1928, 185.2157);
 	    cv::Mat E, R, t, mask;
 		E = findEssentialMat(obj, scene, focal, pp, cv::RANSAC, 0.999, 1.0, mask);
 		recoverPose(E, obj, scene, R, t, focal, pp, mask);
 		std::cout << "R = "<< std::endl << " "  << R << std::endl << std::endl;
 		std::cout << "t = "<< std::endl << " "  << t << std::endl << std::endl;
 	    
-	    if ( t.at<double>(2) > t.at<double>(0) && t.at<double>(2) > t.at<double>(1) ){
+	    // if ( t.at<double>(2) > t.at<double>(0) && t.at<double>(2) > t.at<double>(1) ){
 
 	    	if (flag){
 				t_f = t;
 				R_f = R;
 				flag = 0;
+				std::cout << "here" << std::endl;
 			}
 			else{
-				t_f = t_f + 0.5*(R_f*t);
+				t_f = t_f + (R_f*t);
 	      		R_f = R*R_f;
 			}
 
-	    }
+	    // }
 		
 	    scene = obj;
+	    // std::cout << "t_f = "<< std::endl << " "  << t_f << std::endl << std::endl;
 		int x = int(t_f.at<double>(0)) + 300;
     	int y = int(t_f.at<double>(2)) + 100;
     	cv::circle(traj, cv::Point(x, y) ,1, CV_RGB(255,0,0), 2);
+
+
     	imshow( "Road facing camera", imgObject );
     	cv::imshow( "Trajectory", traj );
-		cv::waitKey(1);
+		cv::waitKey(0);
 
-	    //cv::imshow("imgFinal", imgFinal);
-	    //cv::waitKey(0);
+	    // cv::imshow("imgFinal", imgFinal);
+	    // cv::waitKey(0);
 	}
     return(0);
 
